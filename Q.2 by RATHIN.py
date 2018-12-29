4992c3e2f037f66175132683c9e0b1fa4ba25352
Rathinchiku <46235908+Rathinchiku@users.noreply.github.com>
2018-12-29 15:51:46 +0530

    Add files via upload

---
+++ Q.2 by RATHIN.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+x=int(input("enter no. of times u want to enter"))
+array=[]
+for y in range(0,x):
+      z=int(input(""))
+      array.append(z)
+print("the minimum of array is",min(array))
+print("the maximum of array is",max(array))
+print("the value of array is",(array))
+
