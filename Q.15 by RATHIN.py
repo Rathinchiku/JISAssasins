812a19a5273b78892e7f7c27a98d31ba60bdb7be
Rathinchiku <46235908+Rathinchiku@users.noreply.github.com>
2018-12-29 15:46:18 +0530

    Add files via upload

---
+++ Q.15 by RATHIN.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# ques no.15
+
+x=int(input("enter value of x "))
+y=int(input("enter value of y "))
+z=int(input("enter value of z "))
+c=y+z
+print("result is ", x**c)
+
+
+# In[ ]:
+
+
+
+
